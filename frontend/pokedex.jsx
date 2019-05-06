fee0df9aac2f89cda0b5ac7c369cdd5c7d63c711
Kevin Lee <pvklee@ucla.edu>
2019-05-06 09:31:06 -0700

    set up webpack and frontend structure

---
+++ frontend/pokedex.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+document.addEventListener('DOMContentLoaded', (e)=>{
+  const root = document.getElementById('root');
+  ReactDOM.render(<h1>HI</h1>, root)
+});
