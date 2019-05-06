0a868528da19544eff0c78679f855babe6fe88db
Kevin Lee <pvklee@ucla.edu>
2019-05-06 10:33:39 -0700

    working pokemon selector

--- frontend/pokedex.jsx
+++ frontend/pokedex.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import configureStore from './store/store'
 import {requestAllPokemon} from './actions/pokemon_actions'
+import {selectAllPokemon} from './reducers/selectors'
 
 document.addEventListener('DOMContentLoaded', (e)=>{
   const root = document.getElementById('root');
@@ -11,6 +12,7 @@ document.addEventListener('DOMContentLoaded', (e)=>{
   window.getState = store.getState;
   window.dispatch = store.dispatch;
   window.requestAllPokemon = requestAllPokemon;
+  window.selectAllPokemon = selectAllPokemon;
   //--- debugging only
 
   ReactDOM.render(<h1>HI</h1>, root)
