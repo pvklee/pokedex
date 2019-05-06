8b9b66481273dcbd4f66cb7935046369f3ba0fea
Kevin Lee <pvklee@ucla.edu>
2019-05-06 13:11:56 -0700

    working requestpokemon

--- frontend/pokedex.jsx
+++ frontend/pokedex.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import configureStore from './store/store'
-import {requestAllPokemon} from './actions/pokemon_actions'
+import {requestAllPokemon, requestPokemon} from './actions/pokemon_actions'
 import {selectAllPokemon} from './reducers/selectors'
 import {fetchPokemon} from './util/api_util'
 import Root from './components/root'
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', (e)=>{
   window.dispatch = store.dispatch;
   window.requestAllPokemon = requestAllPokemon;
   window.selectAllPokemon = selectAllPokemon;
-  window.fetchPokemon = fetchPokemon;
+  window.requestPokemon = requestPokemon;
   //--- debugging only
 
   ReactDOM.render(<Root store={store}/>, root)
